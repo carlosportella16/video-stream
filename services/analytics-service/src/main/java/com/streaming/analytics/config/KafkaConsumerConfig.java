702d4307e2f91e72c7eae4eb8f711f2d2ca314bf
Carlos Portella <carlosportella16@hotmail.com>
2026-03-26 22:08:21 -0300

    feat: Kafka config for analytics service

---
+++ services/analytics-service/src/main/java/com/streaming/analytics/config/KafkaConsumerConfig.java
@@ -0,0 +1,51 @@
+package com.streaming.analytics.config;
+
+import com.streaming.analytics.model.VideoEvent;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class KafkaConsumerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.consumer.group-id:analytics-group}")
+    private String groupId;
+
+    @Bean
+    public ConsumerFactory<String, VideoEvent> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put("bootstrap.servers", bootstrapServers);
+        props.put("group.id", groupId);
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", JsonDeserializer.class);
+        props.put("spring.json.type.mapping", "videoEvent:com.streaming.analytics.model.VideoEvent");
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, "com.streaming.analytics.model.VideoEvent");
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        props.put("auto.offset.reset", "earliest");
+        props.put("enable.auto.commit", true);
+        props.put("auto.commit.interval.ms", 1000);
+
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, VideoEvent> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, VideoEvent> factory =
+            new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+}
+
